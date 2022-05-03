3867700e91d2bd55563e79b2ae60b6bdb53b6c48
prab98 <104759936+prab98@users.noreply.github.com>
2022-05-03 21:29:59 +0530

    Delete _09_Data_Structures 02.05.2022 directory

--- _09_Data_Structures 02.05.2022/_01_numbers/_00_Generic_Functions.py
+++
@@ -1,9 +0,0 @@
-'''
-# Generic Functions
----------------------
-print()  :  Prints the given value
-input()  :  Receives input from end user keyboard
-type()   :  Gives type of variable/value
-id()     :  Gives address of the variable/value
-
-'''
